787993e6e83927562f6db7a755b603a7a56b142e
Nadalvn <97375222+Nadalvn@users.noreply.github.com>
2022-03-20 15:56:34 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,19 @@
+function show() {
+  alert("Lợi ích: Thực hiện thâm canh, xen canh, tăng vụ, phát triển nền nông nghiệp nhiệt đới. - Tác hại: + Nấm mốc, sâu bệnh phát triển.")
+}
+
+function show1() {
+  alert("Nhiệt độ quanh năm đều cao trên 21°c, lượng mưa lớn (1500 – 2000 mm/năm) và độ ẩm không khí trên 80%.")
+}
+
+function show2 () {
+  alert("Cho ta nhiều sản vật, nhất là gỗ. Và rừng cũng có tác dụng điều họa khí hậu, che phủ đất và hạn chế nước mưa tràn về đồng bằng gây lũ lụt.")
+}
+
+function show3() {
+  alert("Hằng năm thường có bão, có năm mưa lớn gây lũ lụt, có năm lại xảy ra hạn hán làm ảnh hưởng đến đời sống.")
+}
+
+function show4() {
+  alert("Trồng rừng, phủ xanh đất trống đồi trọc. Bảo vệ rừng phòng hộ, các vườn quốc gia và khu dự trữ thiên nhiên. Khai thác hợp lý rừng sản xuất, hạn chế khai hoang chuyển rừng thành đất nông nghiệp, hạn chế di dân tự do.")
+}
